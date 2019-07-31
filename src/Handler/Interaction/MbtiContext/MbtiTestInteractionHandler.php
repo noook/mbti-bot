ba20c87a52f820ddb3efb4ce341922419e8a5eaa
noook <me@neilrichter.com>
2019-08-01 00:07:46 +0200

    :art: Formatting messages to answer to a question

--- src/Handler/Interaction/MbtiContext/MbtiTestInteractionHandler.php
+++ src/Handler/Interaction/MbtiContext/MbtiTestInteractionHandler.php
@@ -11,23 +11,27 @@ use App\Handler\Interaction\InteractionHandlerAliases;
 use App\Messenger\MessengerRequestMessage;
 use App\Repository\FacebookUserRepository;
 use App\Repository\MbtiTestRepository;
+use App\Helper\MbtiHelper;
 
 class MbtiTestInteractionHandler implements InteractionHandlerInterface
 {
     private $facebookUserRepository;
     private $mbtiTestRepository;
+    private $mbtiHelper;
     private $em;
     private $translator;
 
     public function __construct(
         FacebookUserRepository $facebookUserRepository,
         MbtiTestRepository $mbtiTestRepository,
+        MbtiHelper $mbtiHelper,
         ObjectManager $em,
         TranslatorInterface $translator
     )
     {
         $this->facebookUserRepository = $facebookUserRepository;
         $this->mbtiTestRepository = $mbtiTestRepository;
+        $this->mbtiHelper = $mbtiHelper;
         $this->em = $em;
         $this->translator = $translator;
     }
@@ -42,26 +46,32 @@ class MbtiTestInteractionHandler implements InteractionHandlerInterface
         $test = $this->mbtiTestRepository->currentTest($user);
         $messages = [];
 
-        if (null === $test) {
-            $test = (new MbtiTest)
-                ->setCompleted(false)
-                ->setStep(1)
-                ->setUser($user);
-            $this->em->persist($test);
-            $this->em->flush();
-
-            $startMessages = ['lets_start_test', 'test_is_40_questions_long', 'answer_like_this'];
-            foreach ($startMessages as $message) {
-                $messages[] = [
-                    'text' => $this->translator->trans($message, [], null, $user->getLocale()),
-                ];
-            }
-        } else if ($test->getStep() > 1) {
+        if ($test->getStep() > 1) {
             $messages[] = [
                 'text' => $this->translator->trans('test_already_started', [], null, $user->getLocale()),
             ];
-        }
+        } else {
+            if (null === $test) {
+                $test = (new MbtiTest)
+                    ->setCompleted(false)
+                    ->setStep(1)
+                    ->setUser($user);
+                $this->em->persist($test);
+                $this->em->flush();
+    
+                $startMessages = ['lets_start_test', 'test_is_40_questions_long', 'answer_like_this'];
+                foreach ($startMessages as $message) {
+                    $messages[] = [
+                        'text' => $this->translator->trans($message, [], null, $user->getLocale()),
+                    ];
+                }
+            }
 
-        dd($messages);
+            $questions = $this->mbtiHelper->getNextQuestion($test);
+            
+            $messages[] = $this->mbtiHelper->prepareQuestion($questions, $user);
+
+            dd($messages);
+        }
     }
 }
--- src/Helper/MbtiHelper.php
+++ src/Helper/MbtiHelper.php
@@ -2,7 +2,74 @@
 
 namespace App\Helper;
 
+use Symfony\Component\Translation\TranslatorInterface;
+use App\Entity\MbtiTest;
+use App\Repository\MbtiQuestionRepository;
+use App\Repository\MbtiTestRepository;
+use App\Entity\FacebookUser;
+
 class MbtiHelper
 {
-    
+    const EMOJI_VOTE = ['🥝', '🍉', '🍎', '🍑', '🍍'];
+
+    private $mbtiQuestionRepository;
+    private $mbtiTestRepository;
+    private $translator;
+
+    public function __construct(
+        MbtiQuestionRepository $mbtiQuestionRepository,
+        MbtiTestRepository $mbtiTestRepository,
+        TranslatorInterface $translator
+    )
+    {
+        $this->mbtiQuestionRepository = $mbtiQuestionRepository;
+        $this->mbtiTestRepository = $mbtiTestRepository;
+        $this->translator = $translator;
+    }
+
+    public function getNextQuestion(MbtiTest $test)
+    {
+        return $this->mbtiQuestionRepository->findBy(['step' => $test->getStep()]);
+    }
+
+    /**
+     * @param MbtiQuestion[] $questions
+     * @return void
+     */
+    public function prepareQuestion(array $questions, FacebookUser $user): array
+    {
+        $emojis = self::EMOJI_VOTE;
+        shuffle($emojis);
+        $emojis = array_slice($emojis, 0, 2);
+
+        $payloads = [
+            [
+                'emoji' => $emojis[0],
+                'question' => $questions[0],
+            ],
+            [
+                'emoji' => $emojis[1],
+                'question' => $questions[1],
+            ],
+        ];
+        $text = implode("\n", [
+            $emojis[0]. ': ' . $this->translator->trans($questions[0]->getStep() . '.' . $questions[0]->getKey(), [], 'mbti', $user->getLocale()),
+            '',
+            $emojis[1]. ': ' . $this->translator->trans($questions[1]->getStep() . '.' . $questions[1]->getKey(), [], 'mbti', $user->getLocale()),
+        ]);
+        shuffle($payloads);
+
+        return [
+            'text' => $text,
+            'quick_replies' => array_map(function ($item) {
+                return [
+                    'title' => $item['emoji'],
+                    'payload' => \json_encode([
+                        'step' => $item['question']->getStep(),
+                        'value' => $item['question']->getValue(),
+                    ]),
+                ];
+            }, $payloads),
+        ];
+    }
 }
